c4b4a634a2dbbd589178b0573e66dfcc021bf62b
alerolon15 <alerolon15@gmail.com>
2016-02-03 19:46:38 -0300

    nuevo package.json

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>Baterias Caden - Taller</h1>
+<p>Sistema para Gestion de Autos del taller</p>
+
+<code>Para empezar a utilizar:</code><br>
+<code>ingresar un dominio y solicitar si existe un registro</code><br>
+<code>En el caso de no haber registro, se mostraran todos los campos vacios</code><br>
+<code>volver hacia atras con el boton "volver"</code><br>
+<code>y empiece un nuevo registro</code><br>
+<code>llene el formulario con todos los datos del auto</code><br>
+<code>(si el cliente requiere comprar una bateria en caden muestre el catalogo con el boton "baterias")</code><br>
+
+<p>ver todos los registros del sistema: </p><br>
+<code>para ver todos los registros de la base de datos</code><br>
+<code>puede entrar a la tabla de registros</code><br>
+<code>ingresando la url: "/tabla"</code><br>
+
+
---
+++ app.js
@@ -0,0 +1,471 @@
+"use strict";
+
+
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var app = express();
+var mongoose = require("mongoose");
+var hbs = require("handlebars");
+var async = require("async");
+var bodyParser = require('body-parser')
+mongoose.connect('mongodb://localhost/autos')
+var db = mongoose.connection;
+var Dominio = "";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/public', express.static('public'));
+app.set('view engine','handlebars');
+app.engine('html',expressHandlebars());
+
+var autoSchema = {
+	Dominio: String,
+	Marca: String,
+	Modelo: String,
+	Combustible: String,
+	Anio: Number,
+	Test: String,
+	Bateria: String,
+	Fecha: String,
+	Mecanico: String
+};
+
+var Auto = mongoose.model("Auto", autoSchema);
+
+var batSchema = {
+	_id: String,
+	marca: String,
+	modelo: String,
+	imagen: String,
+	voltaje: String,
+	amperaje: String,
+	cca: String,
+	rc: String,
+	borne: String,
+	medidas: {alto: String, ancho: String, largo: String},
+	precio: String
+
+};
+
+var Bateria = mongoose.model("Bateria", batSchema);
+
+
+
+app.get('/',function(req,res){
+	Auto.find({},{_id:0,Dominio:1},function(err,autos){
+		if(err){console.log(err)}
+
+		var options = {
+        layout:"layout.html",
+        autos
+    };
+
+    res.render('index.html',options);
+});
+});
+
+
+app.post('/registro',function(req,res){
+	var today = new Date();
+
+	var data = {
+	Dominio: req.body.Dominio,
+	Marca: req.body.Marca,
+	Modelo: req.body.Modelo,
+	Combustible: req.body.Combustible,
+	Anio: req.body.Anio,
+	Test: req.body.Test,
+	Bateria: req.body.Bateria,
+	Fecha: today.toISOString().substring(0, 10),
+	Mecanico: req.body.Mecanico
+	};
+    var options = {
+        layout:"layout.html",
+    };
+    var auto = new Auto(data);
+
+	auto.save(function(err){
+		console.log(auto);
+	});
+    
+    res.render('index.html',options);
+});
+
+app.post('/registro2',function(req,res){
+	var today = new Date();
+
+	var data = {
+	Dominio: req.body.Dominio,
+	Marca: req.body.Marca,
+	Modelo: req.body.Modelo,
+	Combustible: req.body.Combustible,
+	Anio: req.body.Anio,
+	Test: req.body.Test,
+	Bateria: req.body.Bateria,
+	Fecha: today.toISOString().substring(0, 10),
+	Mecanico: req.body.Mecanico
+	};
+    var options = {
+        layout:"dashboard.html",
+    };
+    var auto = new Auto(data);
+
+	auto.save(function(err){
+		console.log(auto);
+	});
+    
+    res.redirect('/admin');
+});
+
+app.get('/consulta',function(req,res){
+    var options = {
+        layout:"layout.html",
+        Dominio: req.body.Dominio,
+        auto: docu[0]
+    };
+    res.render('consulta.html',options);
+});
+
+app.post('/actualizar',function(req,res){
+	Bateria.find({},function(err,docus){
+		if(err){console.log(err)}
+
+	var bats = [];
+	for (var i = docus.length - 1; i >= 0; i--) {
+		bats[i] = docus[i];
+	};
+	var busca = req.body;
+	Auto.find(busca,function(err,docu){
+		if(err){console.log(err)}
+	var options = {
+        layout:"layout.html",
+        Dominio: req.body.Dominio,
+        auto: docu[0],
+        bat: docus,
+        bats
+
+    };
+    res.render('actualizar.html',options);
+	});
+});
+});
+app.post('/actualizarautos',function(req,res){
+	Bateria.find({},function(err,docus){
+		if(err){console.log(err)}
+
+
+	var bats = [];
+	for (var i = docus.length - 1; i >= 0; i--) {
+		bats[i] = docus[i];
+	};
+	var busca = req.body;
+	Auto.find(busca,function(err,docu){
+		if(err){console.log(err)}
+	var options = {
+        layout:"dashboard.html",
+        Dominio: req.body.Dominio,
+        auto: docu[0],
+        bat: docus,
+        bats
+
+    };
+    res.render('actualizar2.html',options);
+	});
+});
+});
+
+app.post('/borrarautos',function(req,res){
+
+	var busca = req.body;
+	Auto.remove(busca,function(err,docu){
+		if(err){console.log(err)}
+
+    res.redirect('/tablaautos');
+	});
+	});
+
+app.post('/borrarbaterias',function(req,res){
+
+	var busca = req.body;
+	Bateria.remove(busca,function(err,docu){
+		if(err){console.log(err)}
+			console.log(docu)
+
+    res.redirect('/tablabaterias');
+	});
+	});
+
+app.post('/actualizarbaterias',function(req,res){
+
+	var busca = req.body;
+	Bateria.find(busca,function(err,docu){
+		if(err){console.log(err)}
+
+	var options = {
+        layout:"dashboard.html",
+        bateria: docu[0]
+    };
+
+    res.render('actualizarbateria.html',options);
+	});
+	});
+
+app.post('/actualizarbat',function(req,res){
+
+	var data = {
+	_id: req.body.Codigo,
+	marca: req.body.Marca,
+	modelo: req.body.Modelo,
+	imagen: "public/images/bateria_portada.png",
+	voltaje: req.body.Voltaje,
+	amperaje: req.body.Amperaje,
+	cca: req.body.CCA,
+	rc: req.body.RC,
+	borne: req.body.Borne,
+	medidas: {alto: req.body.Alto, ancho: req.body.Ancho, largo: req.body.Largo},
+	precio: req.body.Precio
+	};
+
+	Bateria.update({_id: req.body.Codigo},
+		data, function(err){
+			if(err){console.log(err)}
+		});
+
+    res.redirect('/tablabaterias');
+	});
+
+app.post('/actualizado',function(req,res){
+	var today = new Date();
+
+	var data = {
+	Dominio: req.body.Dominio,
+	Marca: req.body.Marca,
+	Modelo: req.body.Modelo,
+	Combustible: req.body.Combustible,
+	Anio: req.body.Anio,
+	Test: req.body.Test,
+	Bateria: req.body.Bateria,
+	Fecha: today.toISOString().substring(0, 10),
+	Mecanico: req.body.Mecanico
+	};
+	
+
+	Auto.update({Dominio: req.body.Dominio},
+		data, function(err){
+			if(err){console.log(err)}
+		});
+
+	var options = {
+        layout:"layout.html"
+    };
+    res.render('index.html',options);
+	});
+
+app.post('/actualizado2',function(req,res){
+	var today = new Date();
+
+	var data = {
+	Dominio: req.body.Dominio,
+	Marca: req.body.Marca,
+	Modelo: req.body.Modelo,
+	Combustible: req.body.Combustible,
+	Anio: req.body.Anio,
+	Test: req.body.Test,
+	Bateria: req.body.Bateria,
+	Fecha: today.toISOString().substring(0, 10),
+	Mecanico: req.body.Mecanico
+	};
+	
+
+	Auto.update({Dominio: req.body.Dominio},
+		data, function(err){
+			if(err){console.log(err)}
+		});
+
+
+	var busca = req.body;
+	Auto.find({},function(err,docu){
+		if(err){console.log(err)}
+
+	var todos = [];
+	for (var i = docu.length - 1; i >= 0; i--) {
+		todos[i] = docu[i];
+	};
+
+	var options = {
+        layout:"dashboard.html",
+        auto: docu,
+        todos
+    };
+    res.render('tablaautos.html',options);
+	});
+	});
+
+app.post('/consulta', function(req,res){
+	var busca = req.body;
+	Auto.find(busca,function(err,docu){
+		if(err){console.log(err)}
+	var urltest = "public/archivo/";
+	var options = {
+        layout:"layout.html",
+        Dominio: req.body.Dominio,
+        auto: docu[0],
+        urltest
+    };
+	res.render('consulta.html',options);
+	
+	});
+});
+
+
+app.get('/registro',function(req,res){
+    Bateria.find({},function(err,docus){
+		if(err){console.log(err)}
+		
+	var options = {
+        layout:"layout.html",
+        bat: docus
+    };
+    res.render('registro.html',options);
+});
+});
+
+app.get('/agregarbaterias',function(req,res){
+    Bateria.find({},function(err,docus){
+		if(err){console.log(err)}
+		
+	var options = {
+        layout:"dashboard.html",
+        bat: docus
+    };
+    res.render('registrobaterias.html',options);
+});
+});
+
+app.post('/agregarbaterias',function(req,res){
+
+	Auto.count({},function(err,dominios){
+		if(err){console.log(err)}
+
+	Bateria.count({},function(err,baterias){
+		if(err){console.log(err)}
+
+	var data = {
+	_id: req.body.Codigo,
+	marca: req.body.Marca,
+	modelo: req.body.Modelo,
+	imagen: "public/images/bateria_portada.png",
+	voltaje: req.body.Voltaje,
+	amperaje: req.body.Amperaje,
+	cca: req.body.CCA,
+	rc: req.body.RC,
+	borne: req.body.Borne,
+	medidas: {alto: req.body.Alto, ancho: req.body.Ancho, largo: req.body.Largo},
+	precio: req.body.Precio
+	};
+    var options = {
+        layout:"dashboard.html",
+        dominios,
+        baterias
+    };
+    var bateria = new Bateria(data);
+
+	bateria.save(function(err){
+		console.log(bateria);
+	});
+    
+    res.render('admin.html',options);
+});
+});
+});
+
+app.get('/admin',function(req,res){
+    
+    Auto.count({},function(err,dominios){
+		if(err){console.log(err)}
+
+	Bateria.count({},function(err,baterias){
+		if(err){console.log(err)}
+
+		
+	var options = {
+        layout:"dashboard.html",
+        dominios,
+        baterias
+    };
+    res.render('admin.html',options);
+});
+});
+});
+
+app.get('/graficos',function(req,res){
+    
+    Auto.count({Bateria:"BAT001"},function(err,dominios){
+		if(err){console.log(err)}
+
+	Bateria.count({},function(err,baterias){
+		if(err){console.log(err)}
+
+		
+	var options = {
+        layout:"dashboard.html",
+        dominios,
+        baterias
+    };
+    res.render('graficos.html',options);
+});
+});
+});
+
+app.get('/agregarautos',function(req,res){
+
+    Bateria.find({},function(err,docus){
+		if(err){console.log(err)}
+		
+	var options = {
+        layout:"dashboard.html",
+        bat: docus
+    };
+
+    res.render('registro2.html',options);
+	});
+});
+
+app.get('/tablaautos',function(req,res){
+
+	var busca = req.body;
+	Auto.find({},function(err,docu){
+		if(err){console.log(err)}
+
+	var todos = [];
+	for (var i = docu.length - 1; i >= 0; i--) {
+		todos[i] = docu[i];
+	};
+		
+	var options = {
+        layout:"dashboard.html",
+        auto: docu,
+        todos
+    };
+
+    res.render('tablaautos.html',options);
+	});
+});
+
+
+app.get('/tablabaterias',function(req,res){
+    Bateria.find({},function(err,docus){
+		if(err){console.log(err)}
+		
+	var options = {
+        layout:"dashboard.html",
+        bat: docus
+    };
+    res.render('tablabaterias.html',options);
+});
+});
+
+app.listen(3000,function(){
+    console.log("SERVER UP");
+});
